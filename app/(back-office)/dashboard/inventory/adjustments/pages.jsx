aa46bef8759a8b6b526ad2904fe3282cc548dcf1
LeJays <jameswalter695@gmail.com>
2025-10-04 17:29:27 +0100

    Test

---
+++ app/(back-office)/dashboard/inventory/adjustments/pages.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ajustments = () => {
+    return (
+        <div>
+            <h2>Adjustement du Stock</h2>
+        </div>
+    );
+}
+
+export default Ajustments;
--- app/(back-office)/dashboard/inventory/page.jsx
+++ app/(back-office)/dashboard/inventory/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>Stock</h2>
+            <h2>Gestion du Stock</h2>
         </div>
     );
 }
