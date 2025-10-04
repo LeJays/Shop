90365fc0aaf2ecdefd09cbf9f2bc152d26ef656c
LeJays <jameswalter695@gmail.com>
2025-10-04 17:31:49 +0100

    Description concise des modifications

--- app/(back-office)/dashboard/inventory/page.jsx
+++ app/(back-office)/dashboard/inventory/page.jsx
@@ -1,8 +1,10 @@
+import FixedHeader from '@/components/dashboard/FixedHeader';
 import React from 'react';
 
 const Inventory = () => {
     return (
         <div>
+            <FixedHeader/>
             <h2>Gestion du Stock</h2>
         </div>
     );
---
+++ components/dashboard/FixedHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FixedHeader = () => {
+    return (
+        <div>
+            <h2>Fixed Header  </h2>
+        </div>
+    );
+}
+
+export default FixedHeader;
